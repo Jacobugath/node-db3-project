15b128e315c1869d5027a4bbec91c2440bd089bc
Jacob Morris <jacobthomasmorris@gmail.com>
2021-02-02 20:13:40 -0700

    done

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,51 @@
 // scheme-model
+const db = require('../../data/db-config');
+
+
+const find = () =>{
+    return db('schemes');
+}
+
+const findById = id =>{
+    return db('schemes')
+    .where('id', id)
+    .first()
+}
+
+const findSteps = id =>{
+
+    
+    return db('schemes')
+    .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+    .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where('schemes.id', id)
+}
+
+const add = scheme =>{
+    return db('schemes')
+    .insert(scheme)
+    .then(a => {return findById(a)})
+}
+
+const remove = id =>{
+    return db('schemes').where('id', id)
+    .del()
+}
+
+const update = (changes, id) => {
+    return db('schemes')
+      .where('id', id)
+      .update(changes)
+      .then(a =>{
+          return findById(id);
+      })
+  }
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    remove,
+    update
+  };
--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,12 @@
 // db-config
+const knex = require('knex');
+
+const config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/schemes.db3',
+  },
+  useNullAsDefault: true,
+};
+
+module.exports = knex(config);
